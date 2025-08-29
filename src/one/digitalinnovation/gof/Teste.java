24c8315d8ef6d83785ce5af7dacf48d01a6fa058
jotave <jotavenog@gmail.com>
2025-08-29 17:10:46 -0300

    first commit

---
+++ src/one/digitalinnovation/gof/Teste.java
@@ -0,0 +1,47 @@
+package one.digitalinnovation.gof;
+
+import one.digitalinnovation.gof.facade.Facade;
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        //Testes Singleton
+
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+
+        //Strategy
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setStrategy(normal);
+
+        robo.mover();
+        robo.mover();
+
+        //Facade
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Joao", "18240000");
+
+    }
+}
